eb0551f28eb6c472f0fe29af9b9489fab51e2af2
Refilwe3533 <Refilwe.mahlanyane@netcare.co.za>
2022-06-02 08:29:38 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,12 @@
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
- *   *
- *
- *    * Description: prints "Programming is like building a multilingual puzzle"
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *       */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
@@ -22,6 +16,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 
 }
