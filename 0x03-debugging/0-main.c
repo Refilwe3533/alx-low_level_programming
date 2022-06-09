63a7397c3cb15e98b2f976f29e6f2e20b05ca75d
Refilwe3533 <Refilwe.mahlanyane@netcare.co.za>
2022-06-09 04:10:09 -0700

    2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 
+
 /**
  * main - tests function that prints if integer is positive or negative
  *
  * Return: 0
- *
  */
-
 
 int main(void)
 {
@@ -17,4 +16,5 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
+
 }
